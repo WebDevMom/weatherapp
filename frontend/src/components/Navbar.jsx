1179412ccf10f663ec42b7012d37617e1bc14c6d
Carmen Smith <cheylaracarm77@gmail.com>
2022-08-14 15:40:44 -0500

    first commit

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Navbar() {
+    return (
+        <div>
+            <ul>
+                <li>Home</li>
+                <li>less</li>
+                <li>People</li>
+            </ul>
+        </div>
+    )
+}
+export default Navbar
